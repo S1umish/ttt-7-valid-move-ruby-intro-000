98a4e016ca1709e1752e7dfd12d7963be18adc3b
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-29 23:36:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,10 +4,10 @@ puts "Enter a position on the board 1-9:"
 def input_to_index (user_input)
   user_input.to_i - 1
   user_input =gets.chomp
-  number_entered = user_input.to_i - 1
+  index = user_input.to_i - 1
 end
-def valid_move?(number_entered)
-    number_entered.between?(0,8) && !(position_taken?(display_board,number_entered))
+def valid_move?(board,index)
+    index.between?(0,8) && !position_taken?(display_board,index)
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
