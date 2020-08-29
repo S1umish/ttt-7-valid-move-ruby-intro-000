65922843bec8ed80d9063fff1b4995cec833e778
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-29 23:41:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,9 +6,9 @@ def input_to_index (user_input)
   user_input =gets.chomp
   index = user_input.to_i - 1
 end
-# def valid_move?(board,index)
-#     index.between?(0,8) && !position_taken?(display_board,index)
-# end
+def valid_move?(board,index)
+    index.between?(0,8) && !position_taken?(display_board,index)
+end
 # # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 # def position_taken?(board, index)
 #   !(board[index] == " " || board[index] == "" || board[index] == nil)
