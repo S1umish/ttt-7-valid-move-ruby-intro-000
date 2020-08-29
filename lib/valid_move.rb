48acb2e9f9912cd92a6ac784a375966966b86a93
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-29 23:23:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
 # code your #valid_move? method here
-
-
+@ttt_board = [" ", "X", " ", " ", " ", " ", " ", " ", " "]
+puts "Enter a position on the board 1-9:"
+def input_to_index (user_input)
+  user_input.to_i-1
+  user_input =gets.chomp
+  number_entered = user_input.to_i - 1
+def valid_move?(number_entered)
+    number_entered.between?(0,8) && !(position_taken?(@ttt_board,number_entered))
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
