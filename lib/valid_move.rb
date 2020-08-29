a932cdb3fa8646d74ea6fb38a0c1af9acfab0299
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-29 23:43:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def input_to_index (user_input)
   index = user_input.to_i - 1
 end
 def valid_move?(board,index)
-    index.between?(0,8) && !position_taken?(display_board,index)
+    index.between?(0,8) && !position_taken?(board, index)
 end
 # # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 # def position_taken?(board, index)
